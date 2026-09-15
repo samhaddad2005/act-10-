c89d5a2f722f6c37026f2c971133a8d955e540a3
Sam Haddad <samhaddad80@gmail.com>
2026-09-15 15:11:01 -0600

    Fix square constructor and perimeter

--- src/square.py
+++ src/square.py
@@ -2,11 +2,15 @@ class Square:
 
     # FIXME
     def __init__(self, side) -> None:
-        pass
+            if side <= 0:
+                self.side = 1
+            else:
+                self.side = side
 
     def area(self): 
         return self.side * self.side 
     
     # FIXME
     def perimeter(self): 
-        return self.side * 2
+        return self.side * 4
+    
